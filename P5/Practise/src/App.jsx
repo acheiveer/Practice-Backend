6068cb1e544356b6a8a2f1d71fff9bee2de0f24e
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-04-30 00:15:30 +0530

    using useEffect for todos

--- P5/Practise/src/App.jsx
+++ P5/Practise/src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios"
 
 function App() {
-return <>
+    const [todos,setTodos] =useState([]);
 
+    useEffect(() => {
+        axios.get("https://sum-server.100xdevs.com/todos")
+        .then(function(response){
+            setTodos(response.data.todos)
+        })
+    }, [])
+    
+return <>
+        {todos.map(todo => <Todo title={todo.title} description={todo.description}/>)}
  </>
+}
+
+function Todo({title,description}){
+    return <>
+        <h1>{title}</h1>
+        {description}
+    </>
 }
 
 
