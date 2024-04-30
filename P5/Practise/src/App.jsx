80f35ebce22907b027cf6b547e93dae4a4aec00e
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-04-30 19:49:11 +0530

    useMemo update

--- P5/Practise/src/App.jsx
+++ P5/Practise/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,22 +8,28 @@ function App() {
     const [count,setCount] = useState(0)
     const [inputval,setInputval] = useState(1)
 
-    var cnt =0;
+    // var cnt =0;
+    // for(let i=1;i<=inputval;i++){
+    //     cnt+=i
+    // }
+
+    let cnt = useMemo(()=>{
+        let cnt =0;
     for(let i=1;i<=inputval;i++){
         cnt+=i
     }
-    const handleChange = (event) => {
-        setInputval(event.target.value);
-      };
+    return cnt;
+    },[inputval]);
+   
 return <>
        <input
-        type="text"
-        value={inputval}
-        onChange={handleChange}
+        onChange={(e)=>{
+            setInputval(e.target.value)
+        }}
         placeholder="Enter No"
       />
       <p>Sum of 1 to {inputval} is {cnt}</p>
-      
+
         <button onClick={()=>{
             setCount(count +1)
         }}>Counter ({count})</button>
