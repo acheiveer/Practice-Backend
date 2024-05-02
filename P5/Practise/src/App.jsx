374929fa34a11e0e3f2d0582c75909178b53a23d
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-02 05:38:49 +0530

    update useRef

--- P5/Practise/src/App.jsx
+++ P5/Practise/src/App.jsx
@@ -5,44 +5,18 @@ import './App.css'
 import axios from "axios"
 
 function App() { 
-  const [exchnage1data,setExchnage1data] = useState({});
-  const [exchnage2data,setExchnage2data] = useState({});
-  const [bankData,setBankData] = useState({})
-
-
-  useEffect(()=>{ 
-        setExchnage1data({returns:100});
-  },[])
-
-  useEffect(()=>{ 
-    setExchnage2data({returns:100});
-},[])
+    const [Incometax,useIncometax] = useState(20000)
 
  useEffect(()=>{
     setTimeout(()=>{
-        setBankData({
-            returns:100
-        });
+      document.getElementById("IncometaxConatiner").innerHTML=10;
       },5000)
  },[])
 
-// useCallback is not about minimizing the amout of code that is run 
-// useCallback is about not rendering a child component, if the function hasnot/doesnpt need to cahnge across render
-
- const calculateCryproReturns = useCallback(function(){
-    return exchnage1data.returns + exchnage2data.returns
- },[exchnage1data,exchnage2data])
-  const incometax = (calculateCryproReturns() + bankData.returns) * 0.3;
-   
-return <>
-      <CryptoGainCalculator  calculateCryproReturns={calculateCryproReturns}/>
+ return <>
+    hio there , your income tax return are <div id="IncometaxConatiner">{Incometax}</div>
  </>
 }
 
-function CryptoGainCalculator({calculateCryproReturns}){
-    return <div>
-        your crypto returns are {calculateCryproReturns()}
-    </div>
-}
 
 export default App
