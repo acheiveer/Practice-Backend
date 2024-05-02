c53b7e495bae3c5fccff658aaf5a68a9ba51a440
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-02 05:41:48 +0530

    useRef Final

--- P5/Practise/src/App.jsx
+++ P5/Practise/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,15 +6,17 @@ import axios from "axios"
 
 function App() { 
     const [Incometax,useIncometax] = useState(20000)
+    const divRef = useRef();
 
  useEffect(()=>{
     setTimeout(()=>{
-      document.getElementById("IncometaxConatiner").innerHTML=10;
+        console.log(divRef.current)
+      divRef.current.innerHTML=10;
       },5000)
  },[])
 
  return <>
-    hio there , your income tax return are <div id="IncometaxConatiner">{Incometax}</div>
+    hio there , your income tax return are <div ref={divRef}>{Incometax}</div>
  </>
 }
 
