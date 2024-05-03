128d8191c661ec46332a1f0ee7a44766c96ec107
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-03 19:17:17 +0530

    Lazy Loading

--- P7/src/App.jsx
+++ P7/src/App.jsx
@@ -1,48 +1,50 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
 import './App.css'
-import {Dashboard} from "./components/Dashboard"
-import {Landing} from "./components/Landing"
+import {lazy} from 'react'
+import { Suspense } from 'react'
+// import { Dashboard } from "./components/Dashboard"
+const  Dashboard = lazy(()=> import("./components/Dashboard"))
+// import { Landing } from "./components/Landing"
+const Landing = lazy(()=>import("./components/Landing"))
 
 function App() {
-  
- 
+
+
 
   return (
-  <div>
-   
-  <BrowserRouter>
-  <Appbar/>
-    <Routes>
-      <Route path='/dashboard' element={<Dashboard/>}/>
-      <Route path='/' element={<Landing/>}/>
+    <div>
 
-    </Routes>    
-    </BrowserRouter>
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+          <Route path='/dashboard' element={<Suspense fallback={"loading..."}><Dashboard /></Suspense>} />
+          <Route path='/' element={<Suspense fallback={"loading..."}><Landing /></Suspense>}/>
 
-  </div>
-    
-    
+        </Routes>
+      </BrowserRouter>
+
+    </div>
   )
 }
 
 
-function Appbar(){
+function Appbar() {
   const navigate = useNavigate();
   return <div>
-     <div>
-   <button onClick={()=>{
-    // window.location.href= "/"      // this window.location.href is not the right way it does not do client side routing it actually refersh the page it reload the page 
-    navigate("/")
-   }}>Landing Page</button>
-     
-    <button onClick={()=>{
-      //  window.location.href= "/dashboard"
-      navigate("/dashboard ")
-   }}>Dashboard Page</button>
-  </div>
+    <div>
+      <button onClick={() => {
+        // window.location.href= "/"      // this window.location.href is not the right way it does not do client side routing it actually refersh the page it reload the page 
+        navigate("/")
+      }}>Landing Page</button>
+
+      <button onClick={() => {
+        //  window.location.href= "/dashboard"
+        navigate("/dashboard ")
+      }}>Dashboard Page</button>
+    </div>
   </div>
 
 }
--- P7/src/components/Landing.jsx
+++ P7/src/components/Landing.jsx
@@ -1,6 +1,6 @@
 
 
-export function Landing(){
+export default function Landing(){
     return <>
         Landing Page
     </>
