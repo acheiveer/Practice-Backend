22bdc1e75f3cd814bad77c04652bc5938c3e5ab8
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-03 18:46:30 +0530

    Another way of doing routing using useNavigate

--- P7/src/App.jsx
+++ P7/src/App.jsx
@@ -1,26 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {BrowserRouter, Route, Routes } from "react-router-dom"
+import {BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
 import './App.css'
 import {Dashboard} from "./components/Dashboard"
 import {Landing} from "./components/Landing"
 
 function App() {
+  
  
 
   return (
-  <>
-  <>
-   <button onClick={()=>{
-    window.location.href= "/"
-   }}>Landing Page</button>
-     
-    <button onClick={()=>{
-       window.location.href= "/dashboard"
-   }}>Dashboard Page</button>
-  </>
+  <div>
+   
   <BrowserRouter>
+  <Appbar/>
     <Routes>
       <Route path='/dashboard' element={<Dashboard/>}/>
       <Route path='/' element={<Landing/>}/>
@@ -28,10 +22,29 @@ function App() {
     </Routes>    
     </BrowserRouter>
 
-  </>
+  </div>
     
     
   )
+}
+
+
+function Appbar(){
+  const navigate = useNavigate();
+  return <div>
+     <div>
+   <button onClick={()=>{
+    // window.location.href= "/"      // this window.location.href is not the right way it does not do client side routing it actually refersh the page it reload the page 
+    navigate("/")
+   }}>Landing Page</button>
+     
+    <button onClick={()=>{
+      //  window.location.href= "/dashboard"
+      navigate("/dashboard ")
+   }}>Dashboard Page</button>
+  </div>
+  </div>
+
 }
 
 export default App
