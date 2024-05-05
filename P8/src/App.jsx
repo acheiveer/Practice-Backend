8db67be208593d73843a189b6f9662220517a926
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-06 01:19:06 +0530

    Recoil

--- P8/src/App.jsx
+++ P8/src/App.jsx
@@ -1,37 +1,36 @@
-import { useContext, useMemo, useState } from "react"
+import { useContext } from "react"
 import { CountContext } from "./context";
 import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import { countAtom, evenSelector } from "./store/atoms/count";
+import { countAtom } from "./store/atoms/count";
 
 function App() {
-  const [count,setCount] = useState(0)
+ 
 
   return (
     <div>
-    <CountContext.Provider value={count}>
-    <Count setCount={setCount}/>
-    </CountContext.Provider>
-   
+    <RecoilRoot>
+      <Count/>
+    </RecoilRoot>
+    
     </div>
   )
 }
-function Count({setCount}){
-  console.log("count render ")
+function Count(){
   return <div>
    <CountRenderer/>
-   <Buttons setCount={setCount}/>
+   <Buttons />
   </div>
 
 }
 
 function CountRenderer(){
-  const count = useContext(CountContext);
+  const count =useRecoilValue(countAtom);
   return <div>
    <b> {count}</b>
   </div>
 }
-function Buttons({setCount}){
-  const count = useContext(CountContext)
+function Buttons(){
+  const [count, setCount] = useRecoilState(countAtom)
   return <>
     <button onClick={()=>{
       setCount(count+1)
