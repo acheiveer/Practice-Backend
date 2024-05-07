624a0f7596d53c53c9208c1cf9141236b8a7035c
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-07 18:49:02 +0530

    Aynchronous data query

--- P10/7.4-async-queries/src/App.jsx
+++ P10/7.4-async-queries/src/App.jsx
@@ -2,8 +2,7 @@
 import './App.css'
 import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 import { notifications, totalNotificationSelector } from './atoms'
-import { useEffect } from 'react'
-import axios from 'axios'
+
 
 function App() {
   return <RecoilRoot>
@@ -15,13 +14,7 @@ function MainApp() {
   const [networkCount, setNetworkCount] = useRecoilState(notifications)
   const totalNotificationCount = useRecoilValue(totalNotificationSelector);
 
-  useEffect(() => {
-    // fetch
-    axios.get("https://sum-server.100xdevs.com/notifications")
-      .then(res => {
-        setNetworkCount(res.data)
-      })
-  }, [])
+
 
   return (
     <>
